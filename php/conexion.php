c79132cb58b70a66f99116c8dda0fbeec65dbc14
Luis <luisagp2005@gmail.com>
2025-11-27 09:53:39 -0600

    Conexión cambiante

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "root";
-$db   = "laboratorio_electronica";
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Ahora puedes usar las variables
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$db   = $_ENV['DB_NAME'];
 
 $conn = new mysqli($host, $user, $pass, $db);
 
